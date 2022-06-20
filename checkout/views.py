e91e3cc2068be2aed5834e5d9757ef10734d8ecd
Ivana505 <ivana.iles@protonmail.com>
2022-06-20 23:36:27 +0000

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,7 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
 
 @require_POST
 def cache_checkout_data(request):
@@ -20,12 +21,11 @@ def cache_checkout_data(request):
             'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
-        return HttpResponse(statu=200)
+        return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
-
 
 
 def checkout(request):
@@ -48,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -101,7 +105,7 @@ def checkout(request):
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
-
+            
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
